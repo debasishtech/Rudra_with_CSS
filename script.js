e39e56ec97d656a45f3b98a9615d743e559d2b8f
debasishtech <debasish.palkitravels@gmail.com>
2026-02-24 15:55:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Rudra Interior Website Loaded");
+// MOBILE MENU
+document.querySelector(".menu-toggle").onclick = function(){
+  document.querySelector(".menu").classList.toggle("show");
+};
+
+// PROJECT FILTER
+function filter(category){
+  let items = document.querySelectorAll(".project");
+
+  items.forEach(item=>{
+    item.style.display =
+      (category==="all" || item.classList.contains(category))
+      ? "block"
+      : "none";
+  });
+}
+
+// LIGHTBOX
+function openLightbox(img){
+  document.getElementById("lightbox").style.display="flex";
+  document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox(){
+  document.getElementById("lightbox").style.display="none";
+}
